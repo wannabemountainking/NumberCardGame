0226625251c1e920a18e3f07fdfde1deaae7ed94
김도윤 <kdy0096@gmail.com>
2025-06-27 01:37:19 +0900

    second chance

--- NumberCardGame/main.swift
+++ NumberCardGame/main.swift
@@ -26,31 +26,49 @@ func FindIndicesOfMinValues(from array: [Int]) -> [Int] {
 // 동일한 최소거리를 갖는 주어진 배열이 있는지 확인하는 함수
 // 매개변수: 조사대상 array
 // return 값: Bool
-
 func hasTwoMinValues(from array: [Int]) -> Bool {
     guard !array.isEmpty, let minValue = array.min() else { return false}
     let numOfMinValues = array.filter { $0 == minValue }.count
     return numOfMinValues == 2
 }
 
-func ifCardNumberIsBiggerThanCardGroupAppending() {
+// Participant 구조체의 인스턴스 3개의 배열에서 각 인스턴스의 속성(cardNumber: Int)을 오름차순으로 정렬한 순서 대로 각 인스턴스의 다른 속성(priority: Int)에 각각 1, 2, 3을 주는 함수( -> 객체를 cardNumber 순으로 나열하고 각각의 priority는 배열의 index + 1을 입력)
+// 매개변수:byCardNumber participants: inout [Participant]
+// return값: [Participant]
+func assignPrioritiesToParticipants(byCardNumber participants: inout [Participant]) {
+    let ascendingParticipants = participants.sorted { $0.cardNumber < $1.cardNumber }
+    for (index, participantSorted) in ascendingParticipants.enumerated() {
+        // 참조복사본 참가자 인스턴스들에 priority를 입력하고
+        let priority = index + 1
+        
+        // 참조복사본 참가자 인스턴스와 이름이 동일한 참가자 인스턴스의 participants 인덱스를 확보하고 해당 참가자 인스턴스의 priority 업데이트
+        guard let i = participants.firstIndex(where: { $0.name == participantSorted.name }) else { fatalError("인덱스를 찾지 못했어요") }
+        participants[i].updatePriority(newValue: priority)
+    }
     
 }
-
 
 struct Participant {
     
     let name: String
-    var cardNumber: Int = 0
-    var priority: Int = 0
-    var distance: Int = NUMBERS[99]
+    var cardNumber: Int
+    var priority: Int
+    var distance: Int
     var cardGroupAppending: CardGroup?
     var cardGroup: CardGroup?
-    var penalty: Int = 0
+    var penalty: Int
     
     static var countOfInitializing = 0  // 참가자 객체가 몇번 생성되었나
     
-    init() {
+    init(name: String, cardNumber: Int = 0, priority: Int = 0, distance: Int = 100, cardGroupAppending: CardGroup? = nil, cardGroup: CardGroup? = nil, penalty: Int = 0) {
+        self.name = name
+        self.cardNumber = cardNumber
+        self.priority = priority
+        self.distance = distance
+        self.cardGroupAppending = cardGroupAppending
+        self.cardGroup = cardGroup
+        self.penalty = penalty
+        
         Self.countOfInitializing += 1
     }
     
@@ -70,7 +88,9 @@ struct Participant {
         penalty += newValue
     }
     
-    mutating func comapare(pariticant: Participant, cardGroups: [CardGroup?]) {
+    
+    // 거리 계산 및 동일 최소값의 경우 가장 큰 last 값을 가진 인덱스 변환
+    mutating func decideCardGroup(from cardGroups: [CardGroup?]) ->Int? {
         
         // A 조건
         // 최소거리 파악 함수
@@ -88,8 +108,11 @@ struct Participant {
         var minDistanceCardGroupIndex: Int?
         
         if hasTwoMinValues {
-            let indices = FindIndicesOfMinValues(from: distances)  // 최소값 인덱스들 배열
-            minDistanceCardGroupIndex = indices.sorted {         // 최소값 중에서 cardGroups의 마지막 요소가 가장 큰 값인 경우의 cardGroups 인덱스
+            // 최소값 인덱스들 배열
+            let indices = FindIndicesOfMinValues(from: distances)
+            
+            // 최소값 중에서 cardGroups의 마지막 요소가 가장 큰 값인 경우의 cardGroups 인덱스
+            minDistanceCardGroupIndex = indices.sorted {
                 let lastMin0 = cardGroups[$0]?.cards.last ?? -1
                 let lastMin1 = cardGroups[$1]?.cards.last ?? -1
                 return lastMin0 < lastMin1
@@ -99,25 +122,34 @@ struct Participant {
                 (distance == minDistance) ? index : nil
             }).last
         }
+        self.distance = minDistance
+        self.cardGroupAppending = cardGroups[minDistanceCardGroupIndex ?? -1]
         
-        guard let minDistanceCardGroupIdx = minDistanceCardGroupIndex else { fatalError("최소값 카드 그룹 인덱스가 사라졌어요.")}
-        distance = minDistance
-        cardGroupAppending = cardGroups[minDistanceCardGroupIdx] // 속성 업데이트
+        return minDistanceCardGroupIndex
+    }
+    
+    
+    // 조건 비교 후 append or 벌점처리와 cardGroup 업데이트
+    mutating func applyToCardGroup(index: Int, cardGroups: inout [CardGroup]) {
         
-        /*
         // B조건: 이것은 play()에서 해결해야 할 듯
 
         // 해당 참가자 객체의 저장된 배열(주어진 배열과 동일)의 마지막 요소 숫자가 참가자가 제시한 숫자보다
         //   가) 크면: 해당 참가자 객체의 저장된 배열에 참가자가 제시한 숫자를 append 한다
         //   나) 작으면: 해당 참가자 객체의 저장된 배열의 크기(size)를 참가자 객체 속성인 panalty에 담고 그 객체에 저장된 배열을 비우고, 주어진 배열에서 해당 배열(value)과 key 값을 제거한다.
         
-        guard let cardGroupAppendingLastNumber = self.cardGroupAppending?.cards.last else {fatalError("cardGroupAppending속성이 없어요")}
+        guard let lastNumberOfCardGroup = cardGroups[index].cards.last else {fatalError("cardGroup에 숫자가 없어요")}
         
-        if self.cardNumber < cardGroupAppendingLastNumber {
-            
+        if self.cardNumber < lastNumberOfCardGroup {
+            cardGroupAppending?.appendCards(newValue: self.cardNumber)
+            guard let aCardGroup = cardGroupAppending else {fatalError("cardGroupAppending이 nil입니다.")}
+            cardGroups[index] = aCardGroup
+        } else {
+            guard let count = cardGroupAppending?.cards.count else {fatalError("cardGroupAppending이 이상합니다.")}
+            self.updatePenalty(newValue: count)
+            cardGroupAppending?.deActivateThisCardGroup()
+            cardGroups[index].deActivateThisCardGroup()
         }
-        */
-
     }
 }
 
@@ -144,7 +176,7 @@ struct CardGroup {
     }
 }
 
-let NUMBERS = Array(1...100)
+
 var cardGroups = [
     CardGroup(id: 10, cards: [10]),
     CardGroup(id: 30, cards: [30]),
@@ -153,118 +185,42 @@ var cardGroups = [
 ]
 
 
-
-
-
-
-
 func play(cardsArr: [Int]) -> [String: Int] {
     
+    // 이중배열로 매개변수 분리
     let cardsChunkedByThree = cardsArr.chunked(by: 3)
     
-    var participants: [Participant] = [
-        Participant(name: "A"),
-        Participant(name: "B"),
-        Participant(name: "C")
-    ]
+    // 참가자 객체 생성
+    var participantA = Participant(name: "A")
+    var participantB = Participant(name: "B")
+    var participantC = Participant(name: "C")
     
+    // 참가자 객체가 1 turn 플레이 하는 것을 반복함
     for cards in cardsChunkedByThree {
-        participants[0].cardNumber = cards[0]
-        participants[1].cardNumber = cards[1]
-        participants[2].cardNumber = cards[2]
         
-        guard let cardNumMin = cards.min(by: { $0 < $1 }),
-              let cardNumMax = cards.max(by: {$0 < $1}) else {fatalError("매개변수 문제발생")}
+        // 종료 조건: 구현해야 함
         
-        let cardNumMiddle = cards.filter({$0 != cardNumMin && $0 != cardNumMax})[0]
+        // 참가자가 각자 내놓은 카드 숫자를 각 객체에 입력
+        participantA.cardNumber = cards[0]
+        participantB.cardNumber = cards[1]
+        participantC.cardNumber = cards[2]
         
-        participants = participants.map { participant in
+        var participants = [participantA, participantB, participantC]
+        // 참가자의 숫자의 크기 순으로 participant 인스턴스의 속성(priority) 업데이트(함수)
+        assignPrioritiesToParticipants(byCardNumber: &participants)
+        
+        // participants를 priority 순으로 배열 만들기
+        participants.sort { $0.priority < $1.priority }
+        
+        for participant in participants {
             var person = participant
-            switch participant.cardNumber {
-            case cardNumMin: person.updatePriority(newValue: 1)
-            case cardNumMiddle: person.updatePriority(newValue: 2)
-            case cardNumMax: person.updatePriority(newValue: 3)
-            default: fatalError("에러입니다.")
+            if let shortestDistanceIndex = person.decideCardGroup(from: cardGroups) {
+                person.applyToCardGroup(index: shortestDistanceIndex, cardGroups: &cardGroups)
             }
-            return person
-        }
-        
-        let participantsDict = [
-            participants[0].priority: participants[0],
-            participants[1].priority: participants[1],
-            participants[2].priority: participants[2]
-        ]
-        
-        // 두 배열의 숫자 대조
-        for i in 1...3 {
-            guard var partaker = participantsDict[i] else {fatalError("참가자 배열을 확인하세요.")}
-            
-            var idx = 0
-            
-            for j in 0...2 {
-                if (participants[j].name == partaker.name) {
-                    idx = j
-                }
-            }
-            
-            
-            var distance = 0
-            
-            print("참여자: \(partaker)")
-            for (key, arr) in cardNumDicts {
-                
-                var value = arr
-                guard let lastElem = value.last else {fatalError("\(value)")}
-                guard let cardNumDict = cardNumDicts[key] else {fatalError("cardNumDicts를 확인하세요")}
-                distance = lastElem - partaker.cardNumber
-                
-                partaker.updateArrayIndex(newValue: key)
-                
-                // 배열 마지막 요소와 가장 가까운 배열에 숫자 append
-                if distance < partaker.distance {
-                    partaker.updateDistance(newValue: distance)
-                    
-                    if value.count > cardNumDict.count {
-                        value[value.count - 1] = partaker.cardNumber
-                    } else if value.count == cardNumDict.count {
-                        value.append(partaker.cardNumber)
-                    }
-                    
-                    partaker.updateArrayBelongsTo(newValue: value)
-                }
-                print("최종 배열과 비교후 partaker: \(partaker)")
-            }
-            if partaker.arrayBelongTo[partaker.arrayBelongTo.count - 1] > partaker.arrayBelongTo[partaker.arrayBelongTo.count - 2] {
-                partaker.updatePenalty(newValue: partaker.arrayBelongTo.count - 1)
-                cardNumDicts.removeValue(forKey: partaker.arrayBelongTo[0])
-                partaker.arrayBelongTo = []
-            } else {
-                cardNumDicts[partaker.arrayBelongTo[0]] = partaker.arrayBelongTo
-            }
-            participants[idx] = partaker
-        }
-        
-        
-        if cardNumDicts.isEmpty {
-            var result: [String: Int] = [:]
-            
-            participants.forEach { participant in
-                result[participant.name] = participant.penalty
-            }
-            return result
         }
     }
-    
-    for participant in participants {
-        print(participant)
-    }
-    
-    var result: [String: Int] = [:]
-    
-    participants.forEach { participant in
-        result[participant.name] = participant.penalty
-    }
-    print(result)
-    return result
+    return [participantA.name: participantA.penalty, participantB.name: participantB.penalty, participantC.name: participantC.penalty]
 }
-play(cardsArr: [55,8,29,13,7,61])
+
+print(play(cardsArr: [21, 9, 4]))
+      
